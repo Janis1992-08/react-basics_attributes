b29bbaa858f404ff78b8a5d7455643add20fd1ed
Janis Schiller <schillerjanis@gmail.com>
2023-11-13 12:06:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,5 +2,10 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
-}
+  return <article className="article">
+    <h2 className="article__title">Headline</h2>
+    <input id="connected"></input>
+    <label htmlFor="connected"></label>
+    <a className="article__link" href="https://de.wikipedia.org/wiki/React">Reade Me</a>
+  </article>;
+};
