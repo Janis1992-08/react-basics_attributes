175f1e0c6a9edc591eb025317bfc81dd6e1d846d
Janis Schiller <schillerjanis@gmail.com>
2023-11-13 12:17:00 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <article className="article">
-    <h2 className="article__title">Headline</h2>
-    <input id="connected"></input>
-    <label htmlFor="connected"></label>
-    <a className="article__link" href="https://de.wikipedia.org/wiki/React">Reade Me</a>
-  </article>;
+  return <Article />
 };
+
+
+function Article() {
+  return (<article className="article">
+  <h2 className="article__title">Headline</h2>
+  <input id="connected"></input>
+  <label htmlFor="connected"></label>
+  <a className="article__link" href="https://de.wikipedia.org/wiki/React">Reade Me</a>
+</article>);
+}
