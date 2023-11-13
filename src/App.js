302f8ed44c6c7e508e7ff626a59be4e51d071b64
Janis Schiller <schillerjanis@gmail.com>
2023-11-13 12:29:43 +0100

    new

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
 function Article() {
   return (<article className="article">
   <h2 className="article__title">Headline</h2>
-  <input id="connected"></input>
+  <input id="connected" type="text"></input>
   <label htmlFor="connected"></label>
   <a className="article__link" href="https://de.wikipedia.org/wiki/React">Reade Me</a>
 </article>);
